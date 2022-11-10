dd0a1ec4f87aec1563331df968113933483836f0
BrunoBenkendorf <brunobenkendorf11@gmail.com>
2022-11-10 16:56:48 -0300

    Entity Cliente

---
+++ projetofinal/src/main/java/br/univille/projetofinal/entity/Cliente.java
@@ -0,0 +1,74 @@
+package br.univille.projetofinal.entity;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+@Entity
+public class Cliente {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String CPF;
+    @Column(length = 1000, nullable = false)
+    private String nome;
+    private String login;
+    private String senha;
+    private boolean jaComprou;
+    @Temporal(value = TemporalType.DATE)
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private Date dataCadastro;
+    
+    public long getId() {
+        return id;
+    }
+    public void setId(long id) {
+        this.id = id;
+    }
+    public String getCPF() {
+        return CPF;
+    }
+    public void setCPF(String cPF) {
+        CPF = cPF;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public String getLogin() {
+        return login;
+    }
+    public void setLogin(String login) {
+        this.login = login;
+    }
+    public String getSenha() {
+        return senha;
+    }
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+    public boolean isJaComprou() {
+        return jaComprou;
+    }
+    public void setJaComprou(boolean jaComprou) {
+        this.jaComprou = jaComprou;
+    }
+    public Date getDataCadastro() {
+        return dataCadastro;
+    }
+    public void setDataCadastro(Date dataCadastro) {
+        this.dataCadastro = dataCadastro;
+    }
+
+
+}
