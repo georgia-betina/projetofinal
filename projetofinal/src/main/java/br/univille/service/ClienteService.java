d2820db588d41ec697d8ccab6a138739f70fea7f
BrunoBenkendorf <brunobenkendorf11@gmail.com>
2022-11-16 17:13:50 -0300

    Cliente Controller

---
+++ projetofinal/src/main/java/br/univille/service/ClienteService.java
@@ -0,0 +1,12 @@
+package br.univille.service;
+
+import java.util.List;
+
+import br.univille.projetofinal.entity.Cliente;
+
+public interface ClienteService {
+    List<Cliente> getAll();
+    Cliente save (Cliente cliente);
+    Cliente findById (long id);
+    List <Cliente> findByNome (String nome);
+}
---
+++ projetofinal/src/main/java/br/univille/service/impl/ClienteServiceImpl.java
@@ -0,0 +1,5 @@
+package br.univille.service.impl;
+
+public class ClienteServiceImpl {
+    
+}
