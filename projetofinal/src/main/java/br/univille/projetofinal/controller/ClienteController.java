9db21b00f4233746ab71760d94c0d9a7e12fffc3
BrunoBenkendorf <brunobenkendorf11@gmail.com>
2022-11-17 16:34:08 -0300

    Entity Fornecedor

--- projetofinal/src/main/java/br/univille/projetofinal/controller/ClienteController.java
+++ projetofinal/src/main/java/br/univille/projetofinal/controller/ClienteController.java
@@ -1,16 +1,16 @@
-package br.univille.projetofinal.controller;
+ package br.univille.projetofinal.controller;
 
 
 import java.util.HashMap;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
+ import org.springframework.beans.factory.annotation.Autowired;
+ import org.springframework.stereotype.Controller;
+ import org.springframework.web.bind.annotation.GetMapping;
+ import org.springframework.web.bind.annotation.RequestMapping;
+ import org.springframework.web.servlet.ModelAndView;
 
-import br.univille.projetofinal.entity.Cliente;
-import br.univille.projetofinal.service.ClienteService;
+ import br.univille.projetofinal.entity.Cliente;
+ import br.univille.projetofinal.service.ClienteService;
 
 @Controller
 @RequestMapping("/clientes")
