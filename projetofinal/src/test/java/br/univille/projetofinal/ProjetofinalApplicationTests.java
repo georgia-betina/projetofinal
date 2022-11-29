326dd69f2038208ef9e410a33a494a608637d09d
BrunoBenkendorf <brunobenkendorf11@gmail.com>
2022-11-29 15:19:19 -0300

    Pop-up

--- projetofinal/src/test/java/br/univille/projetofinal/ProjetofinalApplicationTests.java
+++ projetofinal/src/test/java/br/univille/projetofinal/ProjetofinalApplicationTests.java
@@ -1,21 +1,18 @@
 package br.univille.projetofinal;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
 
-import br.univille.projetofinal.api.ClienteControllerAPI;
+import br.univille.projetofinal.controller.ClienteController;
 
 @SpringBootTest
 @AutoConfigureMockMvc
-class ProjetofinalApplicationTests {
+class ProjetofinalApplicationTests{
 
 	@Autowired
-	private ClienteControllerAPI clienteControllerAPI;
-	@Test
-	void contextLoads() {
-
-	}
-
+	private ClienteController clienteController;
+	@Autowired
+	private MockMvc mockMvc;
 }
